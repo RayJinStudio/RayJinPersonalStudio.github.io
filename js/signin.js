1333bb6446919b360557f7a0b025edb538018ff1
RayJinPersonalStudio <41432965+RayJinPersonalStudio@users.noreply.github.com>
2018-08-13 20:39:45 +0800

    Add files via upload

---
+++ js/signin.js
@@ -0,0 +1,22 @@
+       function readtext() {
+ 
+           Bmob.initialize("7e8ce97c90d7014bb2cc0e0a2e9b5260", "49b35cbc0308f74a39391c382d2c176a");
+
+            var user = document.getElementById("user").value;
+           
+ 
+       var paw = document.getElementById("paw").value;
+ 
+			Bmob.User.logIn(user, paw, {
+  success: function(user) {
+    // Do stuff after successful login.
+	alert("登陆成功");
+  },
+  error: function(user, error) {
+    // The login failed. Check error to see why.
+	alert("Error: " + error.code + " " + error.message);
+  }
+});
+           
+        }
+
---
+++ js/signup.js
@@ -0,0 +1,40 @@
+function signup() {
+ 
+           Bmob.initialize("7e8ce97c90d7014bb2cc0e0a2e9b5260", "49b35cbc0308f74a39391c382d2c176a");
+ var user1 = document.getElementById("user").value;
+           
+ 
+       var paw = document.getElementById("paw").value;
+ var paw2 = document.getElementById("paw2").value;
+ if(paw.length>8)
+ {
+ if(paw==paw2)
+ {
+			var user = new Bmob.User();
+user.set("username", user1);
+user.set("password", paw);
+
+
+// other fields can be set just like with Bmob.Object
+
+
+user.signUp(null, {
+  success: function(user) {
+	  alert("注册成功");
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
+           }
+		   else{
+			    alert("两次输入密码不一致");
+		   }
+		   }
+		   else{
+			   alert("为了账号安全，密码请大于8位");
+		   }
+        }
+
